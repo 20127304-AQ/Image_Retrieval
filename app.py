73df63ffd77edbaa20ffa3132476a1c3cb8e51fc
20127304_Trần Quang An Quốc <tranquanganquoc@gmail.com>
2023-08-17 00:02:49 +0700

    update template + app

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 import pickle
 import base64
+import numpy as np
+import cv2
+from scipy.spatial import cKDTree
+from sklearn.metrics.pairwise import cosine_similarity
 from module import ImageSearcher
 
 app = Flask(__name__)
@@ -24,6 +28,13 @@ with open(file_path, 'rb') as file:
 
 searcher = ImageSearcher(loaded_dict)
 
+# Extract feature vectors and image paths from the loaded dictionary
+feature_vectors = list(loaded_dict.values())
+image_paths = list(loaded_dict.keys())
+
+# Build a KDTree using the feature vectors
+kdtree = cKDTree(feature_vectors)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     base64_img = ""  # Empty placeholder
@@ -31,21 +42,42 @@ def index():
 
     if request.method == 'POST' and 'photo' in request.files:
         photo = request.files['photo']
-        num_results = int(request.form['num_results']) 
+        num_results = int(request.form['num_results'])
         if photo.filename != '':
             photo_data = photo.read()
-            
+
             # Convert the uploaded image to base64 for displaying
             base64_img = base64.b64encode(photo_data).decode('utf-8')
 
-            # Perform the search
-            search_result = searcher.search(photo_data, num_results)
+            # Convert the photo_data to a numpy array (OpenCV image)
+            nparr = np.frombuffer(photo_data, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+            # Apply Haar Cascade for face detection
+            faces = searcher.detect_faces(img)
+
+            if len(faces) == 1:
+                x, y, w, h = faces[0]
+                roi_color = img[y:y+h, x:x+w]
+                resized = cv2.resize(roi_color, (128, 128))
+                img = resized
+            else:
+                print("No faces detected in the image.")
+
+            # Get the input feature vector
+            input_feat = searcher.feature_extractor.extract(img)
+
+            # Perform k-nearest neighbor search using the KDTree
+            k_nearest_distances, k_nearest_indices = kdtree.query(input_feat, k=num_results)
 
             # Prepare the result for rendering
-            for similarity, image_path in search_result:
+            for distance, idx in zip(k_nearest_distances, k_nearest_indices):
+                image_path = image_paths[idx]
+                similarity = 1 - distance
                 full_path = './preprocessed_dataset/' + image_path
                 base64_image = image_path_to_base64(full_path)
-                result_list.append({'image_path': base64_image, 'similarity': similarity})
+                result_list.append({'image_path': base64_image, 'similarity': similarity,
+                                    'original_image_path': image_path})
 
     return render_template('index.html', result_list=result_list, base64_img=base64_img)
 
